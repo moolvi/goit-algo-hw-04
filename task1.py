e6dad3e60903ed26093a34f7820c7fe82e656cd8
Oleksandr <sanelka77@yahoo.com>
2024-02-06 22:02:38 +0200

    .gitignore, open, read, exceptions, salary, average

---
+++ task1.py
@@ -0,0 +1,14 @@
+def total_salary(path):
+    employees = None
+    try:
+        with open(path, encoding='utf-8') as file:
+            employees = file.read().split('\n')
+        employees = {employee.split(",")[0] : int(employee.split(",")[1]) for employee in employees}
+    except FileNotFoundError:
+        return None
+    except ValueError:
+        return None
+    except IndexError:
+        return None
+    sum_salary = sum(employees.values())
+    return (sum_salary, sum_salary / len(employees) if len(employees) > 0 else 1)
