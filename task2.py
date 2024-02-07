17ff12da68cf2f6fffa02bc6ce754260be6f5f9c
Oleksandr <sanelka77@yahoo.com>
2024-02-07 22:30:56 +0200

    three solutions

--- task2.py
+++ task2.py
@@ -1,11 +1,24 @@
-import re
+#____1____Solution
+#import re
 
 def get_cats_info(path):
     employees = []
     try:
         with open(path, encoding='utf-8') as file:
-            for row in file.readlines():
-                employees += [dict(zip(["id", "name", "age"], re.split('[,\n]', row)))]
+            
+            #____1____Solution
+            #for row in file.readlines():
+                #employees += [dict(zip(["id", "name", "age"], re.split('[,\n]', row)))]
+            
+            #____3____Solution
+            #for row in file.read().split('\n'):
+                #employees += [dict(zip(["id", "name", "age"], row.split(',')))]
+            
+            #____2____Solution
+            key_lists = ["id", "name", "age"]
+            for row in file.read().split('\n'):
+                row = row.split(',')
+                employees +=[{key_lists[i]:row[i] for i in range(min(len(key_lists),len(row)))}]
         return employees
     
     except FileNotFoundError:
